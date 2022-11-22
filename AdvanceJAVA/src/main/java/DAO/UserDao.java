5e3bc36ef306f146e20fa8fc5484dd36e18d88b9
jay-java <jayprajapati.tops@gmail.com>
2022-11-22 17:07:12 +0530

    crud advance

--- AdvanceJAVA/src/main/java/DAO/UserDao.java
+++ AdvanceJAVA/src/main/java/DAO/UserDao.java
@@ -3,6 +3,8 @@ package DAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import connection.DBConnection;
 import model.User;
@@ -62,5 +64,78 @@ public class UserDao {
 			e.printStackTrace();
 		}
 		return u1;
+	}
+	public static List<User> getAllStudents() {
+		ArrayList<User> list = new ArrayList<User>();
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql="select * from student";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				User u1 = new User();
+				u1.setId(rs.getInt("id"));
+				u1.setName(rs.getString("name"));
+				u1.setContact(rs.getLong("contact"));
+				u1.setAddress(rs.getString("address"));
+				u1.setEmail(rs.getString("email"));
+				u1.setPaswsword(rs.getString("password"));
+				list.add(u1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	public static User getDataById(int id) {
+		User u1 = null;
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql="select * from student where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()) {
+				u1 = new User();
+				u1.setId(rs.getInt("id"));
+				u1.setName(rs.getString("name"));
+				u1.setContact(rs.getLong("contact"));
+				u1.setAddress(rs.getString("address"));
+				u1.setEmail(rs.getString("email"));
+				u1.setPaswsword(rs.getString("password"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return u1;
+	}
+	public static void updateUser(User u) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql="update student set name=?,contact=?,address=?,email=?,password=? where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, u.getName());
+			pst.setLong(2, u.getContact());
+			pst.setString(3, u.getAddress());
+			pst.setString(4, u.getEmail());
+			pst.setString(5, u.getPaswsword());
+			pst.setInt(6, u.getId());
+			pst.executeUpdate();
+			System.out.println("data updated");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void deleteUser(int id) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql="delete from student where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			pst.executeUpdate();
+			System.out.println("data deleted");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- AdvanceJAVA/src/main/webapp/home.jsp
+++ AdvanceJAVA/src/main/webapp/home.jsp
@@ -1,26 +1,98 @@
+<%@page import="DAO.UserDao"%>
+<%@page import="java.util.List"%>
 <%@page import="model.User"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Bootstrap Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<%
 	User u = null;
 	if (session.getAttribute("data") != null) {
-		u = (User)session.getAttribute("data");
+		u = (User) session.getAttribute("data");
 	} else {
 		response.sendRedirect("login.jsp");
 	}
 	%>
-	
-	<h1>Welcome, <%=u.getName() %></h1>
-	<h1>Welcome, <%=u.getContact() %></h1>
-	<h1>Welcome, <%=u.getAddress() %></h1>
-	<h1>Welcome, <%=u.getEmail() %></h1>
-	<h1>Welcome, <%=u.getPaswsword() %></h1>
+
+	<h1 class="text-center">
+		Welcome,
+		<%=u.getName()%></h1>
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Name</th>
+					<th>Contact</th>
+					<th>Address</th>
+					<th>Email</th>
+					<th>Password</th>
+					<th>Edit</th>
+					<th>Delete</th>
+				</tr>
+			</thead>
+			<tbody>
+				<%
+				List<User> list = UserDao.getAllStudents();
+				%>
+				<%
+				for (User u1 : list) {
+				%>
+				<tr>
+					<td><%=u1.getId()%></td>
+					<td><%=u1.getName()%></td>
+					<td><%=u1.getContact()%></td>
+					<td><%=u1.getAddress()%></td>
+					<td><%=u1.getEmail()%></td>
+					<td><%=u1.getPaswsword()%></td>
+					<td>
+						<form action="UserController" method="post">
+							<input type="hidden" name="id" value="<%=u1.getId()%>"> 
+							<input type="submit" name="action" value="edit">
+						</form>
+					</td>
+					<td>
+						<form action="UserController" method="post">
+							<input type="hidden" name="id" value="<%=u1.getId()%>"> 
+							<input type="submit" name="action" value="delete">
+						</form>
+					</td>
+				</tr>
+				<%
+				}
+				%>
+			</tbody>
+		</table>
+		
+		<h1><a href="logout.jsp">Logout...</a></h1>
+	</div>
+
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
